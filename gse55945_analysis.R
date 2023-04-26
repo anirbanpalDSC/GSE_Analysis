61518607abedb84381b381d4cb6e494a4ed5307e
anirbanpaldsc <anirbanpal.wbut@gmail.com>
2023-04-25 23:00:41 -0500

    as of 4/25

--- gse55945_analysis.R
+++ gse55945_analysis.R
@@ -34,9 +34,41 @@ affyData <- ReadAffy(celfile.path = data_path)
 # normalize data
 eset.mas5 = mas5(affyData)
 # Expression Values
-exprSet.nologs = exprs(eset.mas5)
+exprSet_nologs = exprs(eset.mas5)
+# Get the column names
+new_colnames <- gsub("^(.*?)_.*$", "\\1", colnames(exprSet_nologs))
+# Assign the new column names to the matrix
+colnames(exprSet_nologs) <- new_colnames
 # List the column (chip) names
-colnames(exprSet.nologs)
+colnames(exprSet_nologs)
 
 # Create heatmap
-heatmap(exprSet.nologs,main="Normalized ME matrix for brain, liver, N=19")
+# heatmap(exprSet_nologs,main="Normalized ME matrix for brain, liver, N=19")
+
+# log transform for further normalization
+exprSet <- log(exprSet_nologs,2)
+
+# Run the Affy A/P call algorithm on the CEL files we processed above
+mas5callsAP <- mas5calls(affyData)
+
+# Get the actual A/P calls
+# Generates matrix for P = Present and A = Absent
+mas5callsAP_calls <- exprs(mas5callsAP)
+
+# Create 3 lists for 3 comparison groups
+grA_ergneg <- c('GSM1348933','GSM1348934','GSM1348935','GSM1348936','GSM1348938','GSM1348939')
+grB_ergpos <- c('GSM1348940','GSM1348941','GSM1348942','GSM1348943','GSM1348944','GSM1348945')
+grC_normal <- c('GSM1348946','GSM1348947','GSM1348949','GSM1348950','GSM1348951','GSM1348952','GSM1348953')
+grD_AandB <- c(grA_ergneg, grB_ergpos)
+
+# Calculate Expression Ratios of genes between different groups
+# Step1:Calculate mean
+grA_mean <- apply(exprSet[,grA_ergneg],1,mean)
+grB_mean <- apply(exprSet[,grB_ergpos],1,mean)
+grC_mean <- apply(exprSet[,grC_normal],1,mean)
+grD_mean <- apply(exprSet[,grD_AandB],1,mean)
+
+# Compare between log transformed sets
+grC_vs_grA <- grC_mean - grA_mean
+grC_vs_grB <- grC_mean - grB_mean
+grC_vs_grD <- grC_mean - grD_mean
